1325c76172389106a8c695c30a4db0fa7df457a3
Amos Tang <amostang23@icloud.com>
2024-11-06 21:16:44 +0800

    feat: Markdown outline basic

--- src/components/Outline.vue
+++ src/components/Outline.vue
@@ -1,17 +1,72 @@
 <script setup>
+import {computed, onMounted, ref, watch} from 'vue';
+import {useStore} from 'vuex';
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+
+const store = useStore();
+const outline = computed(() => store.getters.getOutline);
+const outlineLine = ref([]);
+
+onMounted(() => {
+    flattenTree(outline.value)
+})
+
+const flattenTree = (tree) => {
+    outlineLine.value = []
+    const result = [];
+
+    function traverse(nodes, level = 1) {
+        nodes.forEach(node => {
+            // 将当前节点的 level 添加到节点对象中
+            result.push({
+                ...node,
+                level
+            });
+
+            // 如果当前节点有子节点，递归遍历子节点，level + 1
+            if (node.children && node.children.length > 0) {
+                traverse(node.children, level + 1);
+            }
+        });
+    }
+
+    traverse(tree);
+    console.log(tree, result)
+    outlineLine.value = result;
+}
+
+watch(outline, (newOutline) => {
+    if (newOutline && newOutline.length > 0) {
+        flattenTree(newOutline);
+    }
+}, {immediate: true});
 
 </script>
 
 <template>
-    <div class="outline">
-        <div class="header">大纲</div>
-
+    <div class="outline-main">
+        <header class="header">大纲</header>
+        <div class="outline">
+            <template v-for="item in outlineLine" :key="item.title">
+                <div class="outline-row" :class="{'outline-first': item.level === 1,'outline-second': item.level === 2}"
+                     :style="{ paddingLeft: `${(item.level - 1) * 20}px` }">
+                    <FontAwesomeIcon :icon="['fas', 'chevron-down']" class="outline-title-icon"
+                                     :style="{ color: item.children.length > 0 ? 'inherit' : 'transparent' }"/>
+                    <div class="outline-text">{{ item.title }}</div>
+                </div>
+            </template>
+        </div>
     </div>
 </template>
 
 <style scoped>
-.outline {
+.outline-main {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
     width: 250px;
+    min-width: 250px;
+    max-width: 250px;
     background: var(--left-background);
     border-right: 1px solid var(--theme-divider);
 }
@@ -19,8 +74,6 @@
 .header {
     height: 40px;
     display: flex;
-    flex-direction: row;
-    flex: 1;
     font-size: 14px;
     line-height: 40px;
     align-items: center;
@@ -29,8 +82,60 @@
     letter-spacing: 2px;
 }
 
+.outline {
+    font-size: 14px;
+    box-sizing: border-box;
+    padding: 0 15px 20px 15px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    overflow-y: auto;
+}
+
+.outline-text {
+    height: 16px;
+    line-height: 16px;
+    white-space: nowrap; /* 禁止文本换行 */
+    overflow: hidden; /* 超出部分隐藏 */
+    text-overflow: ellipsis;
+}
+
+.outline-text:hover {
+    border-bottom: 1px solid;
+}
+
+.outline-first {
+    font-weight: 700;
+    font-size: 16px;
+}
+
+.outline-second {
+    font-weight: 500;
+}
+
+.outline-title-icon {
+    font-size: 10px;
+    color: var(--theme-font-color);
+    margin: 0 7px 0 5px;
+}
+
+.outline-row {
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    height: 30px;
+    cursor: pointer;
+    border-radius: 7px;
+    min-height: 30px;
+}
+
+.outline-row:hover {
+    background: var(--left-background-hover);
+}
+
 @media (prefers-color-scheme: dark) {
-    .outline {
+    .outline-main {
         border-right: 1px solid #000;
     }
 }
