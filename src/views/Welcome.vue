f564da5d1bd1bba224c39f567c6afb28f0311189
Amos Tang <amostang23@icloud.com>
2024-11-10 04:22:34 +0800

    pref: Welcome.vue

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,15 +1,36 @@
 <script setup>
+// TODO: 新建文件
+// TODO: 直接打开最近文件
+// TODO: 选择文件夹后的处理
 import router from "../router/index.js";
-import {onMounted} from "vue";
+import {onMounted, ref} from "vue";
+import store from "../store.js";
 
-onMounted(()=>{
-    //router.push('/file-content')
+const recentOpenList = ref([])
+
+onMounted(() => {
+    let address = localStorage.getItem('address');
+    try {
+        address = JSON.parse(address);
+        if (Array.isArray(address)) {
+            recentOpenList.value = address
+        } else {
+            recentOpenList.value = [];
+        }
+    } catch (e) {
+        recentOpenList.value = [];
+    }
 })
 
 const selectFileOrFolder = async () => {
     const result = await window.electronAPI.openFileOrFolder();
-    if (!result.canceled) {
-        localStorage.setItem('address', result);
+    if (result) {
+        recentOpenList.value.unshift(result)
+        if (recentOpenList.value.length > 8) {
+            recentOpenList.value.splice(8); // 删除超过 8 条数据后的所有内容
+        }
+        localStorage.setItem('address', JSON.stringify(recentOpenList.value));
+        await store.dispatch('updateShowOutline', true);
         await router.push('/file-content')
     }
 };
@@ -23,41 +44,71 @@ const selectFileOrFolder = async () => {
                 <div class="title-second">Welcome</div>
             </div>
             <div class="content">
-                <div class="tool content-bar">
+                <div class="content-bar">
                     <div class="content-title">开始</div>
-                    <div class="content-list-left" @click="newFile">
+                    <div class="content-list-left" @click="newFile" title="new markdown file">
                         <font-awesome-icon :icon="['fas', 'file-circle-plus']" class="content-list-icon"/>
                         新建文件
                     </div>
-                    <div class="content-list-left" @click="selectFileOrFolder">
+                    <div class="content-list-left" @click="selectFileOrFolder" title="open file or folder">
                         <font-awesome-icon :icon="['far', 'folder-open']" class="content-list-icon"/>
-                        打开文件夹
+                        打开文件或文件夹
                     </div>
                 </div>
-                <div class="recent content-bar">
+                <div class="content-bar">
                     <div class="content-title">最近</div>
-                    <div class="content-list-right">新建文件</div>
+                    <div v-for="(item,index) in recentOpenList" :key="item.name+index">
+                        <div class="content-row-right" :title="item.path">
+                            <div @click="openItem" class="content-list-left"> {{ item.name }}</div>
+                            <div class="recent-path">{{ item.path }}</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+        <footer>Copyright © 2024 Amos Tang. All rights reserved.</footer>
     </div>
-
 </template>
 
 <style scoped>
+footer {
+    height: 30px;
+    font-size: 12px;
+    font-weight: 200;
+    text-align: center;
+}
+
+.recent-path {
+    color: var(--theme-font-color);
+    width: 100%;
+    font-size: 12px;
+    margin-left: 15px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+
 .content-list-icon {
     margin-right: 10px;
     font-size: 16px;
 }
 
+.content-row-right {
+    height: 35px;
+    line-height: 30px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
 .content-list-left {
-    margin-right: auto;
     height: 35px;
     line-height: 30px;
     display: flex;
     flex-direction: row;
     align-items: center;
     cursor: pointer;
+    margin-right: auto;
     color: var(--theme-color);
     transition: color 0.4s;
 }
@@ -77,7 +128,10 @@ const selectFileOrFolder = async () => {
 .content-bar {
     display: flex;
     flex-direction: column;
-    flex: 1;
+    width: 50%;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
 }
 
 .content {
@@ -91,9 +145,11 @@ hr {
 
 .main {
     display: flex;
+    flex-direction: column;
     box-sizing: border-box;
     flex: 1;
-    justify-content: center;
+    align-items: center;
+    user-select: none;
 }
 
 .welcome {
@@ -101,8 +157,7 @@ hr {
     box-sizing: border-box;
     flex-direction: column;
     flex: 1;
-    padding: 100px;
-    max-width: 1000px;
+    padding-top: 100px;
 }
 
 .title {
@@ -114,5 +169,60 @@ hr {
 .title-second {
     font-size: 24px;
     margin-bottom: 50px;
+}
+
+
+@media (max-width: 480px) {
+    .welcome {
+        width: 80%;
+        padding-top: 70px;
+    }
+
+    .recent-path {
+        display: none;
+    }
+}
+
+@media (min-width: 481px) and (max-width: 600px) {
+    .welcome {
+        width: 80%;
+        padding-top: 85px;
+    }
+
+    .recent-path {
+        display: none;
+    }
+}
+
+@media (min-width: 601px) and (max-width: 768px) {
+    .welcome {
+        width: 80%;
+        padding-top: 100px;
+    }
+}
+
+@media (min-width: 769px) and (max-width: 1024px) {
+    .welcome {
+        width: 80%;
+        padding-top: 100px;
+    }
+
+    footer {
+        font-size: 16px;
+    }
+}
+
+@media (min-width: 1025px) and (max-width: 1366px) {
+    .welcome {
+        width: 75%;
+        padding-top: 90px;
+    }
+}
+
+@media (min-width: 1367px) {
+    .welcome {
+        width: 60%;
+        padding-top: 100px;
+    }
 }
 </style>
